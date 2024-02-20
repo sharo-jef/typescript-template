e5f8adfee536830accf83d6c48531da89852e8ba
sharo-jef <sharo.jef@gmail.com>
2024-02-21 01:27:53 +0900

    update jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,8 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    testPathIgnorePatterns: [
-        './src/config/test.ts',
-    ],
-    collectCoverageFrom: [
-    ],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testPathIgnorePatterns: ['./src/config/test.ts'],
+  collectCoverageFrom: [],
+  coveragePathIgnorePatterns: ['./src/lib/logger.ts'],
 };
